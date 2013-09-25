06bdec66559007cdab2b5dd85bb62a5fb7b8191e
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-09-25 13:21:29 +0200

    started working on the Simpson integration

---
+++ Week2/simpson.cpp
@@ -0,0 +1,10 @@
+// Author:  Dominik Gresch <greschd@phys.ethz.ch>
+// Date:    25.09.2013 13:20:35 CEST
+// File:    simpson.cpp
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
