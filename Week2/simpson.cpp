29fd150f3af1575c73a390deaf4af45cdc48cff1
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-09-25 13:32:49 +0200

    changed the PRE and POST

--- Week2/simpson.cpp
+++ Week2/simpson.cpp
@@ -1,10 +1,14 @@
 // Author:  Dominik Gresch <greschd@phys.ethz.ch>
 // Date:    25.09.2013 13:20:35 CEST
 // File:    simpson.cpp
+// PRE:     Input is a natural number N giving the number of bins (0,Pi) 
+//          will be split into
+// POST:    Output is the integral over (0,Pi) of sin(x) the using the
+//          Simpson integration rule over Nbins.
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    
+    std::cout << "How "
     return 0;
 }
