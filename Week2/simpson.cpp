5c1808d9feb50d3ddcd11b2b68449b60bff00b96
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-09-26 19:12:26 +0200

    Changed the input type of func and other minor changes

--- Week2/simpson.cpp
+++ Week2/simpson.cpp
@@ -1,47 +1,44 @@
 // Author:  Dominik Gresch <greschd@phys.ethz.ch>
 // Date:    25.09.2013 13:20:35 CEST
 // File:    simpson.cpp
-// PRE:     Input is a non-zero natural number N giving the number of bins (0,Pi) 
-//          will be split into
-// POST:    Output is the integral over (0,Pi) of sin(x) the using the
-//          Simpson integration rule over N bins.
+// PRE:     Input is a non-zero natural number N giving the number of bins (0,Pi) will be split into
+// POST:    Output is the integral over (0,Pi) of sin(x) using the Simpson integration rule over N bins.
 
 #include <iostream>
 #include <math.h>
 #include <assert.h>
 
-double func(double x){
+inline double func(const double & x){
     return sin(x);
 }
 
 int main(int argc, char* argv[]) {
     
     // Input of the number of bins N
-    std::cout << "How many bins would you like to use?" << std::endl;
     int N;
+    std::cout << "How many bins would you like to use?" << std::endl;
     std::cin >> N;
     assert(N > 0);   // I didn't want to use unsigned int because a small negative input would produce huge N
     
     // Optional input of different integration limits (un-comment to use)
-    // std::cout << "What is the lower integration limit?" << std::endl;
-    // double a;
-    // std::cin >> a;
-    // 
-    // std::cout << "What is the upper integration limit?" << std::endl;
-    // double b;
-    // std::cin >> b;
-    // assert(b>a);
+    //~ std::cout << "What is the lower integration limit?" << std::endl;
+    //~ double a;
+    //~ std::cin >> a;
+    //~ 
+    //~ std::cout << "What is the upper integration limit?" << std::endl;
+    //~ double b;
+    //~ std::cin >> b;
+    //~ assert(b>a);
     
     // Standard integration limits (comment when using different ones)
-    const double a = 0;
-    const double b = M_PI;
+    const double a(0);
+    const double b(M_PI);
 
     // Computing the step between two bins
     const double step = (b - a) / N;
     
     // Compute the Simpson numerical integration: first, the parts in the middle of the bin
-    // I define a new starting point in the middle of the first bin, does this make sense? (less addition vs. more variables)?
-    double sum = 0;
+    double sum(0);
     const double a_new = a + step / 2;
     for(int i = 0; i < N ; ++i){
         sum += func(a_new + i * step);
@@ -57,7 +54,7 @@ int main(int argc, char* argv[]) {
     sum += (func(a) + func (b)) / 2;
     
     // Output and adding the prefactor step / 3
-    std::cout << "The integral from " << a << " to " <<  b << " is = " << sum * step / 3 << std::endl; 
+    std::cout << "The integral of sin(x) from x = " << a << " to x = " <<  b << " is = " << sum * step / 3 << std::endl; 
     
     return 0;
 }
