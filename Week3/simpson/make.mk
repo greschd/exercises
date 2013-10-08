b81a7fa972b85a5168e586374bc174108f5fab5c
Dominik Gresch <greschd@student.ethz.ch>
2013-10-08 12:43:04 +0200

    I'm batman!

--- Week3/simpson/make.mk
+++ Week3/simpson/make.mk
@@ -1,11 +1,8 @@
 main : main.o libintegrate.a
 	c++ -Wall -std=c++11 $< -L. -lintegrate -o $@
 
-%.o : %.cpp simpson.hpp.gch 
+%.o : %.cpp simpson.hpp
 	c++ -c -Wall -std=c++11 $< 
 
 libintegrate.a : simpson.o
 	ar ruc $@ $^
-
-simpson.hpp.gch : simpson.hpp
-	c++ -c -Wall -std=c++11 $^
