d50fda5fd550bf43f7047e15783375cd4477f1e7
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-10-16 12:40:42 +0200

    Added exercise 1 of week 5

---
+++ week5/build/CMakeFiles/simpson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simpson.dir/main.cpp.o"
+  "CMakeFiles/simpson.dir/simpson.cpp.o"
+  "simpson.pdb"
+  "simpson"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simpson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
