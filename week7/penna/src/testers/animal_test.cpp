e0922df6564d693674d978f7a7cda9ecc359b845
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-11-02 00:23:20 +0100

    corrected genome.cpp and added genome_test.cpp

---
+++ week7/penna/src/testers/animal_test.cpp
@@ -0,0 +1,17 @@
+// Author:  Dominik Gresch <greschd@phys.ethz.ch>
+// Date:    30.10.2013 13:18:29 CET
+// File:    animal_test.cpp
+
+#include <iostream>
+#include <animal.hpp>
+
+using namespace Penna
+
+int main(int argc, char* argv[]) {
+    Animal testsubject;
+    
+    
+    return 0;
+}
+
+
