037353c0ee56a37d98402c272ef06c2b0ff62cb3
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-11-06 17:08:15 +0100

    Implemented z2, no comments yet.

--- week8/z2/src/z2.cpp
+++ week8/z2/src/z2.cpp
@@ -4,7 +4,58 @@
 
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    
-    return 0;
+// Z2 class
+enum Z2 {Plus, Minus};
+
+template<class T>
+T identity_element() { return T(1); }
+
+template<>
+Z2 identity_element() {return Plus;}
+
+Z2 operator*(const Z2 a,const Z2 b) {
+    return (a == b ? Plus : Minus);
+}
+
+std::ostream& operator<<(std::ostream& os,const Z2 a) {
+    os << (a == Plus ? "Plus" : "Minus");
+    return os;
+}
+
+template<class T>
+T operator*(const T a, const Z2 b)
+{
+    return (b == Plus ? a : -a);
+}
+
+template<class T>
+T operator*(const Z2 b, const T a)
+{
+    return (b == Plus ? a : -a);
+}
+
+template<class T>
+T mypow(T a, const unsigned int n)
+{
+    T b = identity_element<T>();
+    for(unsigned int i = 0; i < n; ++i) {
+        b = b * a;
+    }
+    return b;
+}
+
+int main()
+{
+  // some testing: feel free to add your own!
+  std::cout << Plus*Minus << std::endl;
+  std::cout << Plus*-1*Minus << std::endl;
+  std::cout << identity_element<Z2>() << std::endl; /// test identity_element()
+  std::cout << (1.+3.)*mypow(Minus,4) << std::endl;
+  for (unsigned i=0; i<7; i++)
+    std::cout << "Plus^" << i << " = " << mypow(Plus,i) << std::endl;
+  for (unsigned i=0; i<7; i++)
+    std::cout << "Minus^" << i << " = " << mypow(Minus,i) << std::endl;
+  for (unsigned i=0; i<7; i++)
+    std::cout << "2^" << i << " = " << mypow(2.0,i) << std::endl;
+  return 0;
 }
