e0cc0c169afbcd9a1bd3305dc72d00cbf60e0a25
Dominik Gresch <greschd@stud.phys.ethz.ch>
2013-11-23 23:42:12 +0100

    moved timer to a separate namespace, added benchmarking for the set

--- week10/benchmark/main/benchmark.cpp
+++ week10/benchmark/main/benchmark.cpp
@@ -8,15 +8,18 @@
 #include <set>
 #include <algorithm>
 #include <iterator>
+#include <assert.h>
 #include <timer/dg_timer.hpp> /// uses variable name timer
 
 typedef unsigned int size_type;
-typedef unsigned int val_type;
+typedef int val_type;
+typedef std::set<val_type> set_type;
 
 // creates an array of size n, setting the entries to be equal to the index 
 // and then copies it to a container of type T
 template<class T>
 T initialize(size_type const & n) {
+    assert(n >= 0);
     val_type* array = new val_type[n];
     for(size_type i = 0; i < n; ++i) {
         array[i] = i;
@@ -26,36 +29,76 @@ T initialize(size_type const & n) {
     return container;
 }
 
+// initializes the set
+set_type initialize_set(size_type const & n) {
+    assert(n >= 0);
+    val_type* array = new val_type[n];
+    for(size_type i = 0; i < n; ++i) {
+        array[i] = i;
+    }
+    set_type container(array, array + n);
+    return container;
+}
+
 // inserts and erases an element at a random position in the container n times
 template<class T, class G>
 void insert_erase(T & container, size_type const & n, G & rng) {
     size_type N = container.size();
-    std::uniform_int_distribution<size_type> distr(0,N-1);
+    assert(N > 0);
+    std::uniform_int_distribution<size_type> distr(0,N - 1);
     for(size_type i = 0; i < n; ++i) {
         size_type j = distr(rng);
         auto it = container.begin();
         std::advance(it, j);
-        it = container.insert(it, 0);
+        it = container.insert(it, -1);
         container.erase(it);
     }
+}
+
+// executes the timing measurement
+template<class T, class G>
+void measure(size_type const & container_size, size_type const & k, G & rng) {
+    auto container = initialize<T>(container_size);
+    tool::timer.start();
+    insert_erase(container, k, rng);
+    tool::timer.stop();
+    std::cout <<  tool::timer << std::endl;
+}
+
+//timing for sets
+template<class G>
+void measure(size_type const & container_size, size_type const & k, G & rng) {
+    set_type container = initialize_set(container_size);
+    tool::timer.start();
+    insert_erase(container, k, rng);
+    tool::timer.stop();
+    std::cout << tool::timer << std::endl;
+}
+
+template<class G>
+void measure_set(size_type const & container_size, size_type const & k, G & rng) {
+    
 }
 
 int main(int argc, char* argv[]) {
     std::mt19937 rng;
     rng.seed(42);
+    size_type container_size;
+    std::cout << "enter a system size" << std::endl;
+    std::cin >> container_size;
+    size_type k = 1000000;
+    
     // timing the vector
-    auto vector = initialize<std::vector<val_type>>(10000);
-    timer.start();
-    insert_erase(vector, 1000000, rng);
-    timer.stop();
-    std::cout << "Time used with a vector is "  << timer << std::endl;
+    std::cout << "vector timing:" << std::endl;
+    measure<std::vector<val_type>>(container_size, k, rng);
     
     // timing the list
-    auto list = initialize<std::list<val_type>>(10000);
-    timer.start();
-    insert_erase(list, 1000000, rng);
-    timer.stop();
-    std::cout << "Time used with a list is "  << timer << std::endl;
+    std::cout << "list timing:" << std::endl;
+    measure<std::list<val_type>>(container_size, k, rng);
     
+    // timing the set
+    std::cout << "set timing:" << std::endl;
+    measure(container_size, k, rng);
+        
     return 0;
 }
