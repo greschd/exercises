92ad8dfc46b547814f175218e4fe13a708f4eeb2
greschd <greschd@ethz.ch>
2014-11-17 20:52:56 +0100

    30.Commit	modified run.py / run.sh for weak & strong scaling

--- week08/vortex/test/run.py
+++ week08/vortex/test/run.py
@@ -6,8 +6,17 @@
 # File:    run.py
 
 nproc = [1, 2, 4, 6, 8]
-nproc.extend(list(range(12, 49, 4)))
+nproc.extend(list(range(12, 43, 4)))
+nproc.append(48)
+
+n = 10080
+for i in nproc:
+    if(n % i != 0):
+        print(str(n) + ' not divisible by ' + i)
+
 with open("run.sh", "w") as f:
     for i in nproc:
-        f.write("mpiexec -n " + str(i) + " ./a.out 10000 50 > t_" + str(i) + ".txt;\n")
+        f.write("mpiexec -n " + str(i) + " ./a.out 10080 50 > ../res/strong_" + str(i) + ".txt;\n")
+    for i in nproc:
+        f.write("mpiexec -n " + str(i) + " ./a.out + " + str(n / i) + " 50 > ./res/weak_" + str(i) + ".txt;\n")
 
